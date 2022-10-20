c6bdc6693890f31e77f0e88ab66478f3f551c4c3
Teklit Berihu Gereziher <teklit.berihu.gereziher@diadrom.se>
2022-10-20 13:09:47 +0200

    change client name

--- src/bin/ex9-client.rs
+++ src/bin/ex9-client.rs
@@ -12,7 +12,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut socket = TcpStream::connect(("127.0.0.1", 12345))?;
     
     let msg = ClientMessage::Hello {
-        name: "Emil".to_string(),
+        name: "Teklit".to_string(),
     };
     let buffer = bincode::serialize(&msg)?;
     socket.write(&buffer)?;
